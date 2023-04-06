37230198c9beba78dcb406c3862f6edc0d986512
aneesm8 <anees.mohammed2002@gmail.com>
2023-04-06 06:37:18 -0400

    GA analytics

--- src/components/Aboutpage/Skills.js
+++ src/components/Aboutpage/Skills.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import '../Aboutpage/Aboutpage.css'
+import { Helmet } from 'react-helmet';
 import { Container, Row, Col } from 'react-bootstrap'
 
 function Skills() {
   return (
+    
     <Container>
       <div className="skilltitle">
+      <Helmet>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-52QH33QMYW"></script>
+        <script>
+          {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-52QH33QMYW');`}
+        </script>
+      </Helmet>
         <Row>
           <h2 className='text-center'>My Skills</h2>
           <Col className='skillbox' md={7}>
